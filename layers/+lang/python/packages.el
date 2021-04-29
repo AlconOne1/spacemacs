2ac2e96946763a6774bfb7f2bb4d3ce9cea18f74
Sylvain Benner <sylvain.benner@shopify.com>
2021-04-29 00:51:40 -0400

    [shadowenv] Use global shadowenv mode instead of local
    Move the key bindings to global keymap and follow the new SPC V
    key binding convention.

--- layers/+lang/python/packages.el
+++ layers/+lang/python/packages.el
@@ -51,7 +51,6 @@
     (python :location built-in)
     pyvenv
     semantic
-    shadowenv
     sphinx-doc
     smartparens
     stickyfunc-enhance
@@ -458,9 +457,6 @@ fix this issue."
     (condition-case-unless-debug nil
         ad-do-it
       (error nil))))
-
-(defun python/pre-init-shadowenv ()
-  (add-to-list 'spacemacs--shadowenv-modes 'python-mode))
 
 (defun python/pre-init-smartparens ()
   (spacemacs|use-package-add-hook smartparens
